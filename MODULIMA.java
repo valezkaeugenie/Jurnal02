5858b33f52902181e0b8df4bd05eb3e1351f7e2c
unknown <valezkaeugenie@student.telkomuniversity.ac.id>
2021-10-01 08:48:43 +0700

    MODULIMA.java

---
+++ MODULIMA.java
@@ -0,0 +1,8 @@
+public class MODULIMA {
+
+     public static void main(String[] args) {
+	     System.out.println("NAMA : VALEZKA EUGENIE!");
+	     System.out.println("NIM  : 6706213062!");
+	     System.out.println("KELAS: D3RPLA4502!");
+	}
+}	
